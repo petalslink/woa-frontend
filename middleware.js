cff2902d2b50beaf9b4c6c79513de24cb2b760d9
cdeneux <christophe.deneux@gmail.com>
2014-09-11 10:31:58 +0200

    Fix a deprecation warning about the token management: forbidden access

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
       console.log('Validating token : ', req.query.token);
       console.log('Configured token : ', config.token);
       if (!req.query.token || req.query.token !== config.token) {
-        return res.send(403);
+	return res.status(403).end()
       }
       return next();
     },
